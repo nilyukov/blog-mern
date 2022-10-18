c2a1f3f556746a302db83f0fdf456f3f6581a2d8
Nikita Ilyukov <nilyukov@offsiteteam.com>
2022-10-18 17:35:15 +0300

    isEditable post by author

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,7 @@ import {fetchPosts, fetchTags} from "../redux/slices/posts";
 export const Home = () => {
     const dispatch = useDispatch();
     const {posts, tags} = useSelector(state => state.posts);
+    const userData = useSelector(state => (state.auth.data));
 
     const isPostsLoading = posts.status === 'loading';
 
@@ -40,7 +41,7 @@ export const Home = () => {
                                 viewsCount={obj.viewsCount}
                                 commentsCount={3}
                                 tags={obj.tags}
-                                isEditable
+                                isEditable={userData?._id === obj.user._id}
                             />
                         )
                     )}
